74e35199a22e277bf4a043a4fb4f6ee0c2b9b213
SamarRamanand <95527064+SamarRamanand@users.noreply.github.com>
2022-01-20 17:30:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+rover_width = 100;
+rover_height = 90;
+
+//img_image="Alpkey.png";
+//function add();
+
+
+
+background_image="mars.jpg";
+rover_image="rover.png";
+
+rover_X = 10;
+rover_Y = 10;
+function add(){
+    background_imageTag=new Image();
+    background_imageTag.onload= uploadBackground;
+    background_imageTag.src=background_image;
+
+    rover_imgTag=new Image();
+    rover_imgTag.onload = uploadRover;
+    rover_imgTag.src=rover_image;
+}
+function uploadBackground(){
+    ctx.drawImage(background_imageTag, 0 , 0 , canvas.width,canvas.height);
+}
+
+   // function my_keydown(e){
+//keyPressed = e.keyCode;
+//console.log(keyPressed);
+    
+
+//if((keyPressed >=97 && keyPressed<=122)) (keyPressed >=65 &&keyPressed >=90);
+
+   // {
+//alphabetkey();
+//document.getElementById("d1").innerHTML="You pressed Alphabet Key"
+//}
+function uploadRover(){
+    ctx.drawImage(rover_imgTag, rover_X, rover_Y, rover_width, rover_height);
+
+}
+window.addEventListener("keydown", my_keydown); 
+function my_keydown(e){
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed =='38')
+    {
+        up();
+        console.log("up");
+        
+    }
+    
+    if(keyPressed =='40')
+    {
+        down();
+        console.log("down");
+        
+    }
+    if(keyPressed =='37')
+    {
+        left();
+        console.log("left");
+        
+    }
+    if(keyPressed =='39')
+    {
+        right();
+        console.log("right");
+        
+    }
+}
+
+function up(){
+    if (rover_Y >=0){
+        rover_Y = rover_Y - 10;
+        console.log("when up arrow is pressed, x = " + rover_X + " | y = " + rover_Y);
+        uploadBackground();
+        uploadRover();
+    }
+}
+function down(){
+    if (rover_Y <=500){
+        rover_Y = rover_Y + 10;
+        console.log("when down arrow is pressed,x = " + rover_X + " | y = " + rover_Y);
+        uploadBackground();
+        uploadRover();
+    }
+}
+
+function left(){
+    if (rover_X >=0){
+        rover_X = rover_X - 10;
+        console.log("when left arrow is pressed, x = " + rover_X + " | y = " + rover_Y);
+        uploadBackground();
+        uploadRover();
+    }
+}
+
+function right(){
+    if (rover_X <=700){
+        rover_X = rover_X + 10;
+        console.log("when right is pressed,x = " + rover_X + " | y = " + rover_Y);
+        uploadBackground();
+        uploadRover();  
+}
+}
